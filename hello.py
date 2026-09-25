38ec0c3de8b5788044403cd497a7632c8732d8aa
Danila.Kargajev <danila.kargajev@vkg.ee>
2026-09-25 13:15:43 +0300

    feat: accept name from command line

--- hello.py
+++ hello.py
@@ -1,6 +1,10 @@
+import sys
+
+
 def greet(name: str) -> str:
     return f"Hello, {name}!"
 
 
 if __name__ == "__main__":
-    print(greet("GitHub"))
+    name = sys.argv[1] if len(sys.argv) > 1 else "GitHub"
+    print(greet(name))
